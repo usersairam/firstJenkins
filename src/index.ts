b2059843a10598039e8fdd2765bd2d076da3ea90
Saiarm <98445515+usersairam@users.noreply.github.com>
2025-05-20 17:58:34 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.listen(3000, () => console.log('System is running at 3020'));
 
-app.get('/health-check', (req, res) => res.status(200).json({status:200, message: "Service is up and running!!"}));
+app.get('/health-check', (req, res) => res.status(200).json({status:200, message: "Service is up and running. Health has been updated now!!"}));
 
 
 export default app
